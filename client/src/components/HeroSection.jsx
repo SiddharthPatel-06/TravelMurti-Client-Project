be8287dbbf231cceb2f751efbc8821b41a126100
Siddharth Patel <connectwithsiddh@gmail.com>
2024-10-29 02:00:35 +0530

    modify Herosection and add url for each slider's and css modification

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,33 +1,50 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const heroData = [
-  // {
-  //   id: 1,
-  //   image:
-  //     "https://res.cloudinary.com/djrxcdfrr/image/upload/v1730076608/Jagannatha_Temple-transformed_oqwev2.jpg",
-  //   title: "Holiday Tour",
-  //   description: "Explore the best holiday destinations around the world.",
-  // },
+  {
+    id: 1,
+    image:
+      "https://res.cloudinary.com/djrxcdfrr/image/upload/v1730076611/kadarnath-transformed_n2uz2w.jpg",
+    title: "Chardham Yatra Pilgrimage & Religious Tours",
+    description: "Sacred Visit to Badrinath, Kedarnath, Gangotri & Yamunotri.",
+    link: "/subpackages/671e8f16a52b50bdeacc4896",
+  },
   {
     id: 2,
     image:
-      "https://res.cloudinary.com/djrxcdfrr/image/upload/v1730076611/kadarnath-transformed_n2uz2w.jpg",
-    title: "Spiritual Tour",
-    description: "Connect with your inner peace on our spiritual tours.",
+      "https://akshardham.com/gujarat/wp-content/uploads/2017/04/slider1.jpg",
+    title: "Gujarat Tours Packages",
+    description:
+      "In Gujarat all temples are very famous for their worship and pilgrims.",
+    link: "/subpackages/671e8537a52b50bdeacc42f7",
   },
   {
     id: 3,
     image:
-      "https://images.unsplash.com/photo-1601999705946-fbf42c3c6c66?auto=format&fit=crop&q=100&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGhvbmV5bW9vbiUyMHZhY2F0aW9ufGVufDB8fDB8fHww",
-    title: "Honeymoon Tour",
-    description: "Experience the perfect honeymoon with our special packages.",
+      "https://plus.unsplash.com/premium_photo-1697729444936-8c6a6f643312?&auto=format&fit=crop&q=100&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c291dGglMjBpbmRpYW4lMjB0ZW1wbGVzfGVufDB8fDB8fHww",
+    title: "South Indian Temple Tour",
+    description:
+      "Experience the spiritual essence of South India, known for its ancient and revered temples",
+    link: "/subpackages/671e8b84a52b50bdeacc472d",
   },
   {
     id: 4,
     image:
-      "https://akshardham.com/gujarat/wp-content/uploads/2017/04/slider1.jpg",
-    title: "Gujarat Tours Packages",
-    description: "Experience the perfect honeymoon with our special packages.",
+      "https://plus.unsplash.com/premium_photo-1697730481640-114d8546ef3d?&auto=format&fit=crop&q=100&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODl8fGhpbWFjaGFsJTIwdGVtcGxlfGVufDB8fDB8fHww",
+    title: "Himachal Tours Packages",
+    description:
+      "Explore Himachal's majestic landscapes, from lush valleys to snow-capped peaks.",
+    link: "/subpackages/671eab9aa52b50bdeacc5851",
+  },
+  {
+    id: 5,
+    image:
+      "https://plus.unsplash.com/premium_photo-1697730277839-440df1a4415f?&auto=format&fit=crop&q=100&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8a2FzaG1pcnxlbnwwfHwwfHx8MA%3D%3D",
+    title: "Kashmir Tours Packages",
+    description:
+      "Discover the beauty of Kashmir, with its serene lakes, gardens, and breathtaking views.",
+    link: "/subpackages/671e58c8ead46f3494f89191",
   },
 ];
 
@@ -40,43 +57,39 @@ const HeroSection = () => {
       setCurrentIndex((prevIndex) =>
         prevIndex === heroData.length - 1 ? 0 : prevIndex + 1
       );
-    }, 2000); // Change every 5 seconds
+    }, 2000); // Change every 2 seconds
     return () => clearInterval(slider);
   }, []);
 
+  const currentSlide = heroData[currentIndex];
+
   return (
     <section className="relative w-full h-[calc(100vh-250px)] md:h-[calc(100vh-80px)] bg-gray-800">
-      {/* Image Slider */}
-      {heroData.map((slide, index) => (
-        <div
-          key={slide.id}
-          className={`absolute top-0 left-0 w-full h-full transition-opacity duration-1000 ${
-            index === currentIndex ? "opacity-100" : "opacity-0"
-          }`}
-        >
-          <img
-            src={slide.image}
-            alt={slide.title}
-            className="w-full h-full object-cover"
-            style={{ objectPosition: "center" }}
-          />
-          {/* Centered Component for Info */}
-          <div className="absolute inset-0 flex items-center justify-center">
-            <div className="bg-black bg-opacity-50 p-6 sm:p-8 rounded-lg shadow-lg text-center w-full lg:max-w-4xl md:max-w-2xl max-w-xs">
-              <h2 className="text-2xl sm:text-4xl font-bold text-white">
-                {slide.title}
-              </h2>
-              <p className="mt-4 text-base sm:text-lg text-gray-300">
-                {slide.description}
-              </p>
+      {/* Render only the current slide */}
+      <div className="absolute top-0 left-0 w-full h-full transition-opacity duration-1000 opacity-100">
+        <img
+          src={currentSlide.image}
+          alt={currentSlide.title}
+          className="w-full h-full object-cover"
+          style={{ objectPosition: "center" }}
+        />
+        <div className="absolute inset-0 flex items-center justify-center md:pt-24 pt-16">
+          <div className="bg-black bg-opacity-50 p-6 sm:p-8 rounded-lg shadow-lg text-center w-full lg:max-w-4xl md:max-w-2xl max-w-64">
+            <h2 className="text-2xl sm:text-[28px] font-bold text-white">
+              {currentSlide.title}
+            </h2>
+            <p className="mt-4 text-sm sm:text-base text-gray-200">
+              {currentSlide.description}
+            </p>
+            <Link to={currentSlide.link}>
               <button className="mt-6 px-4 sm:px-6 py-2 sm:py-3 bg-blue-600 text-white font-medium rounded hover:bg-blue-700 transition">
                 Know More
               </button>
-            </div>
+            </Link>
           </div>
         </div>
-      ))}
-      {/* Indicators (Optional) */}
+      </div>
+      {/* Dots indicator */}
       <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex space-x-2">
         {heroData.map((_, index) => (
           <div
