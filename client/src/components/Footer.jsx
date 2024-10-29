f853e2a8cb20d1fa8ce3fa3e0c6549393385c0d6
Siddharth Patel <connectwithsiddh@gmail.com>
2024-10-30 03:30:41 +0530

    fix: Update getDealOfTheDay function to return first subpackage for all Deals of the Day
    - Modified the `getDealOfTheDay` function to fetch and return the first subpackage of each SubPackage marked as "Deal of the Day."
    - Enhanced the code to handle cases where multiple Deals of the Day exist, ensuring all relevant subpackages are returned.
    - Improved error handling to return meaningful messages when no Deals or subpackages are found.
    
    This update resolves the issue of only returning a single subpackage and provides a comprehensive response for all relevant Deals of the Day.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,6 +5,7 @@ import {
   FaInstagram,
   FaLinkedinIn,
 } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   const contactInfo = [
@@ -44,38 +45,40 @@ const Footer = () => {
     },
   ];
 
-  const quicklLinks = [
-    "Home",
-    "Why us?",
-    "About Us",
-    "Travel Tips",
-    "Travel Insurance",
-    "Contact Us",
-    "We are Hiring",
+  const quickLinks = [
+    { name: "Home", path: "/" },
+    { name: "Why us?", path: "/about" },
+    { name: "About Us", path: "/about" },
+    { name: "Travel Tips", path: "/" },
+    { name: "Travel Insurance", path: "/" },
+    { name: "Contact Us", path: "/contact" },
+    { name: "We are Hiring", path: "/" },
   ];
 
   const legalLinks = [
-    "Privacy Policy",
-    "Terms & conditions",
-    "Cancellation & Refund Policy",
-    "Tour Package Booking and T & C",
+    { name: "Privacy Policy", path: "/" },
+    { name: "Terms & Conditions", path: "/" },
+    { name: "Cancellation & Refund Policy", path: "/" },
+    { name: "Tour Package Booking T&C", path: "/" },
   ];
 
   return (
     <footer className="bg-gray-800 text-white border-t border-gray-300">
       <div className="max-w-screen-xl mx-auto px-4 pt-8 pb-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-           {/* Column 1: Additional Links */}
-           <div className="p-4">
+          {/* Column 1: Additional Links */}
+          <div className="p-4">
             <h2 className="text-xl font-bold mb-4">Quick Links</h2>
             <div className="border-b border-gray-300 mb-4"></div>
             <ul className="space-y-2 text-gray-700">
-              {quicklLinks.map((link, index) => (
-                <li
-                  key={index}
-                  className="cursor-pointer text-white hover:text-blue-600 transition duration-200"
-                >
-                  {link}
+              {quickLinks.map((link, index) => (
+                <li key={index}>
+                  <Link
+                    to={link.path}
+                    className="cursor-pointer text-white hover:text-blue-600 transition duration-200"
+                  >
+                    {link.name}
+                  </Link>
                 </li>
               ))}
             </ul>
@@ -97,18 +100,19 @@ const Footer = () => {
             </ul>
           </div>
 
-         
           {/* Column 3: Contact Us */}
           <div className="p-4">
             <h2 className="text-xl font-bold mb-4">Legal</h2>
             <div className="border-b border-gray-300 mb-4"></div>
             <ul className="space-y-2 text-gray-700">
-              {legalLinks.map((service, index) => (
-                <li
-                  key={index}
-                  className="cursor-pointer text-white hover:text-blue-600 transition duration-200"
-                >
-                  {service}
+              {legalLinks.map((link, index) => (
+                <li key={index}>
+                  <Link
+                    to={link.path}
+                    className="cursor-pointer text-white hover:text-blue-600 transition duration-200"
+                  >
+                    {link.name}
+                  </Link>
                 </li>
               ))}
             </ul>
@@ -118,7 +122,7 @@ const Footer = () => {
           <div className="p-4">
             <h2 className="text-xl font-bold mb-4">Contact Us</h2>
             <div className="border-b border-gray-300 mb-4"></div>
-            
+
             <ul className="space-y-2 text-gray-700">
               {contactInfo.map((info, index) => {
                 let contactLink;
